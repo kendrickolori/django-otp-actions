7b36d0b49d0b9a5719e319e0895b980d44a21b1a
kendrick <kendrickolori2@gmail.com>
2025-12-23 17:00:52 +0100

    refactor for pypi publishing

--- otp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import health, verify
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("generate/", health, name="health"),  # Renamed for clarity
-    path("verify/", verify, name="verify"),
-]
